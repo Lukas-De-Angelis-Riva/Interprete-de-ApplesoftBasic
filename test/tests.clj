0d98d6a20fe900b88ec3c93ffaef9d82869b25d4
Lukas DAR <ldeangelis@fi.uba.ar>
2021-02-07 23:49:10 -0300

    Agrega test de la función 'ejecutar-asignacion'

--- test/tests.clj
+++ test/tests.clj
@@ -234,4 +234,15 @@
     (is (= (extraer-data '(())) '()))
 )
 
+(defn test-ejecutar-asignacion
+	(is (= (ejecutar-asignacion '(X = 5) ['((10 (PRINT X))) [10 1] [] [] [] 0 {}]) 
+			'[((10 (PRINT X))) [10 1] [] [] [] 0 {X 5}]))
+	(is (= (ejecutar-asignacion '(X = 5) ['((10 (PRINT X))) [10 1] [] [] [] 0 '{X 2}])
+			'[((10 (PRINT X))) [10 1] [] [] [] 0 {X 5}]))
+	(is (= (ejecutar-asignacion '(X = X + 1) ['((10 (PRINT X))) [10 1] [] [] [] 0 '{X 2}])
+			'[((10 (PRINT X))) [10 1] [] [] [] 0 {X 3}]))
+	(is (= (ejecutar-asignacion '(X$ = X$ + " MUNDO") ['((10 (PRINT X))) [10 1] [] [] [] 0 '{X$ "HOLA"}])
+			'[((10 (PRINT X))) [10 1] [] [] [] 0 {X$ "HOLA MUNDO"}]))
+)
+
 (run-tests)
