713dd9300956a56845e65ace75abdc761ecbaeca
Lukas DAR <ldeangelis@fi.uba.ar>
2021-02-12 23:08:56 -0300

    Revert "Refactoriza las funciones de 'variable?'"
    This reverts commit 614432893c5802406270cfce2e3a682165bfebe3.

--- src/basic.clj
+++ src/basic.clj
@@ -711,7 +711,7 @@
 
 (defn es-posible-nombre-de-variable? [x]
     (and
-        (nil? (re-matches #"(EXIT|ENV|DATA|REM|NEW|CLEAR|LIST|RUN|LOAD|SAVE|LET|AND|OR|INT|SIN|ATN|LEN|MID\$|STR\$|CHR\$|ASC|GOTO|ON|IF|THEN|FOR|TO|STEP|NEXT|GOSUB|RETURN|END|INPUT|READ|RESTORE|PRINT)[$|%]?" (str x)))
+        (nil? (re-matches #"(REM|NEW|CLEAR|LIST|RUN|LOAD|SAVE|LET|AND|OR|INT|SIN|ATN|LEN|MID\$|STR\$|CHR\$|ASC|GOTO|ON|IF|THEN|FOR|TO|STEP|NEXT|GOSUB|RETURN|END|INPUT|READ|RESTORE|PRINT)[$|%]?" (str x)))
         (not (es-numero? x))
         (some? (re-matches #"[A-Z][A-Z0-9]*[$|%]?" (str x)))
     )
@@ -839,9 +839,7 @@
 
 (defn variable-float? [x]
     ;Esto es que sea alfanumerica y la primer letra sea alfabetica.
-    (and
-        (some? (re-matches #"[A-Z]\w*" (str x)))
-        (es-posible-nombre-de-variable? x))
+    ((comp not nil? (partial re-matches #"[a-zA-Z](\w+)?") str) x)
 )
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; variable-integer?: predicado para determinar si un identificador
@@ -854,9 +852,7 @@
 ; false
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn variable-integer? [x]
-    (and
-        (some? (re-matches #"[A-Z]\w*\%" (str x)))
-        (es-posible-nombre-de-variable? x))
+    (= (last (str x)) \%)
 )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -870,9 +866,7 @@
 ; false
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn variable-string? [x]
-    (and
-        (some? (re-matches #"[A-Z]\w*\$" (str x)))
-        (es-posible-nombre-de-variable? x))
+    (= (last (str x)) \$)
 )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
